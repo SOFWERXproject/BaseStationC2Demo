1c9e694d67be03f6117e18f844714833be5ca68d
Barron Stone <7518212+barronstone@users.noreply.github.com>
2018-09-25 12:54:43 -0400

    Update BSClient.java

--- BSClient.java
+++ BSClient.java
@@ -1,8 +1,8 @@
- **
+/**
  * Interface on the phone for communication to/from the base station
  */
  
- interface BSClient implements ActionListener {
+interface BSClient implements ActionListener {
  
     /* configuration methods */
     void config(InetAddres ip, int port);
@@ -15,6 +15,6 @@
     void sendData(BSData d);
     
     /* receive data from base station */
-    
-    
- }
+    BSData recvData(); // get next message from inbound queue
+    public void actionPerformed(ActionEvent e); // TODO - figure out if this is the right place for this... how to tie ActionListen into rest of program?
+}
