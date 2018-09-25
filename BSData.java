66bc93c05bb4a1c73c10bcfc6cbfc039899058d0
Barron Stone <7518212+barronstone@users.noreply.github.com>
2018-09-25 12:46:28 -0400

    Update BSData.java

--- BSData.java
+++ BSData.java
@@ -1,5 +1,5 @@
 /**
- * Container for data being sent to/from the base stations
+ * Container for all data being sent to/from the base stations
  **/
  
 public class BSData implements Serializable {
@@ -7,13 +7,19 @@ public class BSData implements Serializable {
     /* data fields (will include get()/set() for each) */
     String source; // message origin; phone ID or base station
     Enum type;     // identifies if this is a command, type of alert, etc.
-    GPSDataType gpsCoord;
-    TimeStamp time;
-    String Note; // additional notes
-    ImageDataType image;
-    Sound??
+    String additionalNotes;
+    GPSDataType gpsCoord;    // type TBD
+    TimeStamp time;          // type TBD
+    ImageDataType imageFile; // type TBD
+    SoundDataType audioFile; // type TBD
+    VideoDataType videoFile; // type TBD
+ 
+    // ... what other fields might we possibly need???????
     
-    /* serializer methods */
+    /* serializer methods
+     *   if all of the above field are serializable, then we get these for "free" from the Serializable interface,
+     *   otherwise we'll need to manually implement them.
+     */
     public final void writeObject(Object obj) throws IOException;
     public final Object readObject() throws IOException, ClassNotFoundException;
     
